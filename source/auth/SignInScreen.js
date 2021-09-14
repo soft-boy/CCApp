7f480a32763300f6ff99e8122f35e0981edf35c5
Eric Wang <ewang1997@gmail.com>
2021-09-14 15:27:22 -0700

    style auth a lil

--- source/auth/SignInScreen.js
+++ source/auth/SignInScreen.js
@@ -2,6 +2,13 @@ import React, { useContext, useState } from 'react'
 import { View, TextInput, Button } from 'react-native';
 import { FirebaseContext } from './FirebaseProvider'
 
+const InputStyle = {
+  height: 40,
+  margin: 12,
+  borderWidth: 1,
+  padding: 10,
+}
+
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -11,11 +18,13 @@ export default () => {
   return (
     <View>
       <TextInput
+        style={InputStyle}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={InputStyle}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
--- source/auth/SignUpScreen.js
+++ source/auth/SignUpScreen.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, Text } from 'react-native';
 import { FirebaseContext } from './FirebaseProvider'
+
+const InputStyle = {
+  height: 40,
+  margin: 12,
+  borderWidth: 1,
+  padding: 10,
+}
 
 export default ({ navigation }) => {
   const [email, setEmail] = React.useState('');
@@ -11,17 +18,20 @@ export default ({ navigation }) => {
   return (
     <View>
       <TextInput
+        style={InputStyle}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={InputStyle}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
       <Button title="Sign Up" onPress={() => signUp(email, password)} />
+      <Text style={{ alignSelf: 'center' }}>Have an Account?</Text>
       <Button title="Log in" onPress={() => navigation.navigate('SignIn')} />
     </View>
   );
